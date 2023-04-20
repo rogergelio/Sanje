236cf35654658b6cbaf89d3318d4ffa9840d925b
rogergelioo <rogergeliotorres@gmail.com>
2023-04-20 08:38:07 -0600

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
 import { LandingComponent } from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
+import { HowComponent } from './how/how.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
-    LandingComponent
+    LandingComponent,
+    AboutComponent,
+    HowComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LandingComponent {
   
